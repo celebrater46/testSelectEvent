e16f86ac60350954310201690fc3fe36fda0e265
celebrater <tkgm45nbchk@gmail.com>
2022-02-07 14:43:52 +0900

    getting select value succeeded

--- main.js
+++ main.js
@@ -3,4 +3,10 @@
 const h1 = document.getElementById("hw");
 h1.innerHTML = "Hello World from JS"; // succeeded
 
+const select = document.querySelector('[name="member"]');
+
+select.onchange = event => {
+    console.log(select.selectedIndex);
+}
+
 // console.log("hello");
